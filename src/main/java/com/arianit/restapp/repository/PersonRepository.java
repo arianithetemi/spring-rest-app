d35fa13429d4dcce8942f2e89c12bcf66d266bdb
Arianit Hetemi <nitihetemi@gmail.com>
2018-02-26 16:39:09 +0100

    Person Interface Repository and repository package added

---
+++ src/main/java/com/arianit/restapp/repository/PersonRepository.java
@@ -0,0 +1,7 @@
+package com.arianit.restapp.repository;
+
+import com.arianit.restapp.domain.Person;
+import org.springframework.data.repository.CrudRepository;
+
+public interface PersonRepository extends CrudRepository<Person, Long>{
+}
